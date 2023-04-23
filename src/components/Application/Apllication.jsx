9b9d8f20392a19a4cab6c490087ce17a5d5e994c
Alexey Shmelev <alexsey_89@bk.ru>
2023-04-23 20:58:41 +0300

    react content

--- src/components/Application/Apllication.jsx
+++ src/components/Application/Apllication.jsx
@@ -1,9 +1,30 @@
 import styles from './Application.module.scss';
+import React from "react";
 
 function Application(props) {
   return (
-    <div className={styles.app}>{props.date.toLocaleDateString()}</div>
+    <div>
+      <h1>Заголовок 1</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+              Первая часть контента
+          </div>
+          <div className="col-3">
+            Вторая часть контента
+          </div>
+          <div className="col-3">
+            Третья часть контента
+          </div>
+          <div className="col-3">
+            Четвертая часть контента
+          </div>
+        </div>
+      </div>
+    </div>
+    //<div className={styles.app}>{props.date.toLocaleDateString()}</div>
   );
+
 }
 
 export default Application;
