c0cc48d1619e740fcea3b9430437d6c59d31603a
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-24 00:50:45 +0300

    fix components

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,21 +1,17 @@
 import {Card} from "react-bootstrap";
 
-export default function Item() {
-  const title = 'Карточка чего нибудь';
-  const subtitle = 'Заголовок карточки';
-  const date = new Date();
-  const text = 'React-Bootstrap replaces the Bootstrap JavaScript. Each component has been built from scratch as a true React component, without unneeded dependencies like jQuery. As one of the oldest React libraries, React-Bootstrap has evolved and grown alongside React, making it an excellent choice as your UI foundation.';
+export default function Item(props) {
   return (
     <div>
-      <Card bg={"primary"} style={{ width: '18rem' }} border={"light"} text={"white"}>
+      <Card bg={"success"} style={{ width: '18rem' }} border={"light"} text={"white"}>
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Subtitle className="mb-2">{subtitle}</Card.Subtitle>
+          <Card.Title>{props.data.title}</Card.Title>
+          <Card.Subtitle className="mb-2">{props.data.subtitle}</Card.Subtitle>
           <Card.Text>
-            {text}
+            {props.data.text}
           </Card.Text>
           <Card.Footer>
-            {date.toDateString()}
+            {props.data.date}
           </Card.Footer>
         </Card.Body>
       </Card>
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -2,19 +2,43 @@ import Item from "../Item/Item";
 import {Col, Row} from "react-bootstrap";
 
 export default function Items() {
+  const currentDate = (new Intl.DateTimeFormat('ru-RU',{
+    weekday: 'short',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'Europe/Moscow'
+  }).format(Date.now()));
+  const data = [
+    {
+      title: 'Карточка чего нибудь',
+      subtitle: 'Заголовок карточки',
+      date: currentDate,
+      text: 'React-Bootstrap replaces the Bootstrap JavaScript. Each component has been built from scratch as a true React component, without unneeded dependencies like jQuery. As one of the oldest React libraries, React-Bootstrap has evolved and grown alongside React, making it an excellent choice as your UI foundation.'
+    },
+    {
+      title: 'Еще карточка',
+      subtitle: 'Заголовок карточки 2',
+      date: currentDate,
+      text: 'Какой-то другой текст'
+    }
+  ];
   return (
     <Row>
       <Col>
-        <Item></Item>
+        <Item data={data[0]}></Item>
       </Col>
       <Col>
-        <Item></Item>
+        <Item data={data[0]}>></Item>
       </Col>
       <Col>
-        <Item></Item>
+        <Item data={data[1]}>></Item>
       </Col>
       <Col>
-        <Item></Item>
+        <Item data={data[1]}>></Item>
       </Col>
     </Row>
   )
